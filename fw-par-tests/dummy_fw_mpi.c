45ffdbe9a3af090b83557a646a283f54c8965a49
babdary <baturalp.bilgin@ozu.edu.tr>
2024-06-04 20:57:39 +0300

    pseudo app

--- fw-par-tests/dummy_fw_mpi.c
+++ fw-par-tests/dummy_fw_mpi.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+
+
+
+int main(int argc, char* argv[]){
+
+    MPI_Init(NULL,NULL)
+    //data read-> adjacency matrix and partitioning of the data among processes
+}
