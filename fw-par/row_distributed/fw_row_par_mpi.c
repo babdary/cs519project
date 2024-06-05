3b1d557e79dc4879b451f9945313ec7cbd56f9fb
root <root@BABs-ROG>
2024-06-05 22:13:19 +0300

    first commit of new branch

--- fw-par/row_distributed/fw_row_par_mpi.c
+++ fw-par/row_distributed/fw_row_par_mpi.c
@@ -6,6 +6,12 @@
 #include <string.h>
 #include <math.h>
 
+
+
+void floyd warshall(int num_of_vertices){
+
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -17,7 +23,7 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
-    int number_of_vertices;
+    int num_of_vertices;
     char *input_file = argv[1];
 
     if (my_rank == 0)
@@ -31,35 +37,35 @@ int main(int argc, char *argv[])
         {
             printf("Cannot open input file!!!");
         }
-        fscanf(file, "%d", &number_of_vertices);
-        fprintf(stderr, "number of vertices = %d\n", number_of_vertices);
+        fscanf(file, "%d", &num_of_vertices);
+        fprintf(stderr, "number of vertices = %d\n", num_of_vertices);
 
-        double *matrix = (double *)malloc(number_of_vertices * number_of_vertices * sizeof(double));
-        for (int ii = 0; ii < number_of_vertices; ii++)
+        double *matrix = (double *)malloc(num_of_vertices * num_of_vertices * sizeof(double));
+        for (int ii = 0; ii < num_of_vertices; ii++)
         {
-            for (int jj = 0; jj < number_of_vertices; jj++)
+            for (int jj = 0; jj < num_of_vertices; jj++)
             {
-                fscanf(file, "%lf", &matrix[ii * number_of_vertices + jj]);
+                fscanf(file, "%lf", &matrix[ii * num_of_vertices + jj]);
             }
         }
         fclose(file);
-        for (int i = 0; i < number_of_vertices; i++)
+        for (int i = 0; i < num_of_vertices; i++)
         {
-            for (int j = 0; j < number_of_vertices; j++)
+            for (int j = 0; j < num_of_vertices; j++)
             {
-                printf("row %d = %lf\n", i, matrix[i * number_of_vertices + j]);
+                printf("row %d = %lf\n", i, matrix[i * num_of_vertices + j]);
             }
         }
     }
 
     
-    MPI_Bcast(&number_of_vertices, 1, MPI_INT, 0, MPI_COMM_WORLD); // let each proc know the num of vertices
-    int local_iter = number_of_vertices / comm_sz;                 // number of rows each proc will handle
+    MPI_Bcast(&num_of_vertices, 1, MPI_INT, 0, MPI_COMM_WORLD); // let each proc know the num of vertices
+    int local_iter = num_of_vertices / comm_sz;                 // number of rows each proc will handle
     printf("local_iter: %d\n", local_iter);
 
-    double *local_adj_matrix = (double *)malloc(local_iter * number_of_vertices * sizeof(double));
+    double *local_adj_matrix = (double *)malloc(local_iter * num_of_vertices * sizeof(double));
     /*Read Adjacency Matrix of the Weighted Directed Graph from Input File*/
-
     MPI_Finalize();
     return 0;
+
 }
